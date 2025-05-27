9f1364d6ba601709e5ab39c27b50ff3f65fb3c1e
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-27 18:49:14 +0530

    Create employee model

---
+++ day8/employeeapp/src/main/java/lk/ac/vau/fas/ict/model/Employee.java
@@ -0,0 +1,45 @@
+package lk.ac.vau.fas.ict.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Employee extends Person {
+	@Id
+	private String empNo;
+	private double salary;
+	@ManyToOne
+	private Department department;
+	@ManyToMany(mappedBy = "employees")
+	private List<Project>projects;
+	public String getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public List<Project> getProjects() {
+		return projects;
+	}
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+}
